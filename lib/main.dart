2d9c14e33f0b01be3e65d9f0294b05cc66cdb520
manik <mmanik2548@gmail.com>
2024-04-02 22:36:48 +0600

    flutter version 3.19.5 gradle problem solve

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:github_users/data/repositories/details_repository.dart';
 import 'package:github_users/data/repositories/git_user_repository.dart';
 import 'package:github_users/ui/my_home_page.dart';
-
 import 'bloc/details_page_bloc/details_page_bloc.dart';
 import 'bloc/git_user_bloc/git_user_bloc.dart';
 
